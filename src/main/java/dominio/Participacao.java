69e6f3b6d5fd8ddd5c5909e8acd1f1b1b8972c8f
ElianMelo <elianmelomorais@gmail.com>
2019-04-10 16:03:02 -0300

    Implementa a base da classe Participacao

--- src/main/java/dominio/Participacao.java
+++ src/main/java/dominio/Participacao.java
@@ -1,5 +1,77 @@
 package dominio;
 
-public class Participacao {
+import java.io.Serializable;
+import java.math.BigDecimal;
 
+public class Participacao implements Serializable {
+	private static final long serialVersionUID = 1L;
+	Integer codParticipacao;
+	String personagem;
+	BigDecimal desconto;
+	
+	public Participacao() {
+		super();
+	}
+
+	public Participacao(Integer codParticipacao, String personagem, BigDecimal desconto) {
+		super();
+		this.codParticipacao = codParticipacao;
+		this.personagem = personagem;
+		this.desconto = desconto;
+	}
+
+	public Integer getCodParticipacao() {
+		return codParticipacao;
+	}
+
+	public void setCodParticipacao(Integer codParticipacao) {
+		this.codParticipacao = codParticipacao;
+	}
+
+	public String getPersonagem() {
+		return personagem;
+	}
+
+	public void setPersonagem(String personagem) {
+		this.personagem = personagem;
+	}
+
+	public BigDecimal getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(BigDecimal desconto) {
+		this.desconto = desconto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codParticipacao == null) ? 0 : codParticipacao.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Participacao other = (Participacao) obj;
+		if (codParticipacao == null) {
+			if (other.codParticipacao != null)
+				return false;
+		} else if (!codParticipacao.equals(other.codParticipacao))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Participacao [codParticipacao=" + codParticipacao + ", personagem=" + personagem + ", desconto="
+				+ desconto + "]";
+	}
 }
