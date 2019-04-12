4aae0ae6edb4edb9034a26d889daf2f1e6eba54a
ElianMelo <elianmelomorais@gmail.com>
2019-04-12 17:41:14 -0300

    Altera o uso de transacao com banco de dados

--- src/main/java/servico/FilmeServico.java
+++ src/main/java/servico/FilmeServico.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import dao.DaoFactory;
 import dao.FilmeDao;
-import dao.impl.EM;
+import dao.Transaction;
 import dominio.Filme;
 
 public class FilmeServico {
@@ -16,15 +16,29 @@ public class FilmeServico {
 	}
 	
 	public void inserirAtualizar(Filme x) {
-		EM.getLocalEm().getTransaction().begin();
-		dao.inserirAtualizar(x);
-		EM.getLocalEm().getTransaction().commit();
+		try {
+			Transaction.begin();
+			dao.inserirAtualizar(x);
+			Transaction.commit();
+		} catch (RuntimeException e) {
+			if(Transaction.isActive()) {
+				Transaction.rollback();
+			}
+			System.out.println("Erro: " + e.getMessage());
+		}
 	}
 	
 	public void excluir(Filme x) {
-		EM.getLocalEm().getTransaction().begin();
-		dao.excluir(x);
-		EM.getLocalEm().getTransaction().commit();
+		try {
+			Transaction.begin();
+			dao.excluir(x);
+			Transaction.commit();
+		} catch (RuntimeException e) {
+			if(Transaction.isActive()) {
+				Transaction.rollback();
+			}
+			System.out.println("Erro: " + e.getMessage());
+		}
 	}
 	
 	public Filme buscar(int cod) {
--- src/main/java/servico/ParticipacaoServico.java
+++ src/main/java/servico/ParticipacaoServico.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import dao.DaoFactory;
 import dao.ParticipacaoDao;
-import dao.impl.EM;
+import dao.Transaction;
 import dominio.Participacao;
 
 public class ParticipacaoServico {
@@ -16,15 +16,29 @@ public class ParticipacaoServico {
 	}
 	
 	public void inserirAtualizar(Participacao x) {
-		EM.getLocalEm().getTransaction().begin();
-		dao.inserirAtualizar(x);
-		EM.getLocalEm().getTransaction().commit();
+		try {
+			Transaction.begin();
+			dao.inserirAtualizar(x);
+			Transaction.commit();
+		} catch (RuntimeException e) {
+			if(Transaction.isActive()) {
+				Transaction.rollback();
+			}
+			System.out.println("Erro: " + e.getMessage());
+		}
 	}
 	
 	public void excluir(Participacao x) {
-		EM.getLocalEm().getTransaction().begin();
-		dao.excluir(x);
-		EM.getLocalEm().getTransaction().commit();
+		try {
+			Transaction.begin();
+			dao.excluir(x);
+			Transaction.commit();
+		} catch (RuntimeException e) {
+			if(Transaction.isActive()) {
+				Transaction.rollback();
+			}
+			System.out.println("Erro: " + e.getMessage());
+		}
 	}
 	
 	public Participacao buscar(int cod) {
