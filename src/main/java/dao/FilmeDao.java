6b72b5d1ccf428694de407e116d2f29be0e15052
ElianMelo <elianmelomorais@gmail.com>
2019-04-12 15:33:16 -0300

    Cria a camada de acesso a dados

---
+++ src/main/java/dao/FilmeDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.util.List;
+
+import dominio.Filme;
+
+public interface FilmeDao {
+	public void inserirAtualizar(Filme x);
+	public void excluir(Filme x);
+	public Filme buscar(int cod);
+	public List<Filme> buscarTodos();
+}
